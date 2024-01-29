12c786bdbf4735a970341af3ce8f820bba9067a8
Nikolay7350 <154983443+Nikolay7350@users.noreply.github.com>
2024-01-29 18:47:37 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # Елховский Николай, 12-я когорта — Финальный проект. Инженер по тестированию плюс
 
 URL_SERVICE = "https://3207b95b-e8af-461c-89c4-0e149b1cfb07.serverhub.praktikum-services.ru"
-CREAT_ORDERS = "/api/v1/orders"
-TRACK_PATH = "/api/v1/orders/track"
+CREATE_ORDER_PATH = "/api/v1/orders"
+RECEIVING_AN_ORDER_BY_NUMBER = "/api/v1/orders/track?t="
