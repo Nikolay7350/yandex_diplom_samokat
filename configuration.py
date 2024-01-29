f57dacc983d935c3aa258e04983ebb92ccccbb9e
Nikolay7350 <154983443+Nikolay7350@users.noreply.github.com>
2024-01-29 19:06:40 +0300

    Delete configuration.py

--- configuration.py
+++
@@ -1,5 +0,0 @@
-# Елховский Николай, 12-я когорта — Финальный проект. Инженер по тестированию плюс
-
-URL_SERVICE = "https://3207b95b-e8af-461c-89c4-0e149b1cfb07.serverhub.praktikum-services.ru"
-CREATE_ORDER_PATH = "/api/v1/orders"
-RECEIVING_AN_ORDER_BY_NUMBER = "/api/v1/orders/track?t="
